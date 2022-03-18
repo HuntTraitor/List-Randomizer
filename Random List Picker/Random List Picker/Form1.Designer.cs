12593548a48ac6f5ee74717e6276a1d6f5bb360b
HuntTraitor <101893645+HuntTraitor@users.noreply.github.com>
2022-03-18 11:31:06 -0700

    Add files via upload

---
+++ Random List Picker/Random List Picker/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Random_List_Picker
+{
+    partial class rerol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.reroll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select a .txt File ONLY";
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(131, 123);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(115, 20);
+            this.result.TabIndex = 3;
+            this.result.TextChanged += new System.EventHandler(this.result_TextChanged);
+            // 
+            // reroll
+            // 
+            this.reroll.Location = new System.Drawing.Point(141, 90);
+            this.reroll.Name = "reroll";
+            this.reroll.Size = new System.Drawing.Size(90, 27);
+            this.reroll.TabIndex = 4;
+            this.reroll.Text = "Reroll";
+            this.reroll.UseVisualStyleBackColor = true;
+            this.reroll.Click += new System.EventHandler(this.reroll_Click);
+            // 
+            // rerol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 181);
+            this.Controls.Add(this.reroll);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "rerol";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.openFile_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button reroll;
+    }
+}
+
---
+++ Random List Picker/Random List Picker/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Random_List_Picker
+{
+    public partial class rerol : Form
+    {
+
+
+        ArrayList arlist = new ArrayList();
+        int count = 0;
+
+        OpenFileDialog oFile = new OpenFileDialog();
+        string line = "";
+
+        /*
+          
+            CODED MODULES
+         
+         
+         */
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            result.Clear();
+            arlist.Clear();
+            count = 0;
+            if (oFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(oFile.FileName);
+
+                line = sr.ReadLine();
+
+                //case 0
+                if (line == null)
+                {
+                    result.Text = "NO TEXT DETECTED";
+                    return;
+                }
+
+                arlist.Add(line);
+                count++;
+
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    if (line != null)
+                    {
+                        arlist.Add(line);
+                        count++;
+                    }
+                }
+
+                result.Text += arlist[randomNum(count)];
+
+            }
+
+            
+        }
+
+        private void reroll_Click(object sender, EventArgs e)
+        {
+            //case 0
+            if (result.Text == "NO TEXT DETECTED" || result.Text == "")
+                return;
+
+
+            result.Clear();
+            result.Text += arlist[randomNum(count)];
+        }
+
+        public int randomNum(int num)
+        {
+            var randomGenerator = new Random();
+            return randomGenerator.Next(num);
+        }
+
+        /*
+         
+         EMPTY MODULES
+         
+         */
+
+
+        public rerol()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
